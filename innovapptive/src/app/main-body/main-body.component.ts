10f2ebc5e58038b6102504261d311502328fe8f0
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-02-26 15:31:57 +0530

    Right-side-bar segregated

--- innovapptive/src/app/main-body/main-body.component.ts
+++ innovapptive/src/app/main-body/main-body.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LeftSidebarComponent } from '../left-sidebar/left-sidebar.component';
 import { MiddlebodyComponent } from '../middlebody/middlebody.component';
+import { RightSidebarComponent } from '../right-sidebar/right-sidebar.component';
 
 
 @Component({
   selector: 'app-main-body',
   standalone: true,
-  imports: [LeftSidebarComponent, MiddlebodyComponent],
+  imports: [LeftSidebarComponent, MiddlebodyComponent, RightSidebarComponent],
   templateUrl: './main-body.component.html',
   styleUrl: './main-body.component.css'
 })
